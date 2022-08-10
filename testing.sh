07c1615ca18b696b5e94de49f04d53c7aeff3c50
santysanthoshraj <56643213+santysanthoshraj@users.noreply.github.com>
2022-08-10 12:27:52 +0530

    Create testing.sh

---
+++ testing.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+file="./test1.properties"
+while IFS='=' read -r key value
+do
+    key=$(echo $key | tr '.' '_')
+    eval ${key}=\${value}
+done < "$file"
+api_key=${data_api_key}
+app_key=${data_app_key}
+#data = {"monitor_tags":"*","start":+int(currenttime)}
+start=$(date +%s)
+runtime="2 minute"
+end=$(date -ud "$runtime" +%s)
+sleep $(( (RANDOM % 10) + 1 ));
+echo "Muting host monitor..."
+json_val=$(curl -X POST "https://app.datadoghq.com/api/v1/downtime" -H "Content-type: application/json" -H "DD-API-KEY: ${api_key}" -H "DD-APPLICATION-KEY: ${app_key}" -d '{"scope":"cluster-name:cluster-1", "monitor_tags":["compliance:pci","environment:production","product:ls"], "start":"'"$start"'", "end":"'"$end"'", "recurrence": {"type": "days","period": 1}}')
